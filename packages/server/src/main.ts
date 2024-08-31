47a387987cfe6d6b767b3b6e03f683dbfa0e951d
yvrxslvw <yvrxslvv@gmail.com>
2024-09-01 00:05:06 +0300

    Initial commit

---
+++ packages/server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const bootstrap = async () => {
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('/api');
+	await app.listen(3001);
+};
+bootstrap();
