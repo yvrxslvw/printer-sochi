3aa86730a7c7b02caac17e1f28615cff67f0e334
yvrxslvw <yvrxslvv@gmail.com>
2024-09-01 02:21:23 +0300

    Create main page component

--- packages/client/src/app/app.routes.ts
+++ packages/client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { APP_ROUTES } from './shared/constants/routes';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+
+export const routes: Routes = [
+  {
+    path: APP_ROUTES.MAIN_PAGE,
+    component: MainPageComponent,
+  },
+];
---
+++ packages/client/src/app/shared/constants/routes.ts
@@ -0,0 +1,3 @@
+export const enum APP_ROUTES {
+  MAIN_PAGE = '',
+}
