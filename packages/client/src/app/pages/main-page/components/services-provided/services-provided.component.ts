f407cbf31452b1b910320c2d4ee8df32c9f91036
yvrxslvw <yvrxslvv@gmail.com>
2024-09-01 23:25:18 +0300

    Move slider component and create services component

---
+++ packages/client/src/app/pages/main-page/components/services-provided/services-provided.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-provided',
+  standalone: true,
+  imports: [],
+  templateUrl: './services-provided.component.html',
+  styleUrl: './services-provided.component.scss',
+})
+export class ServicesProvidedComponent {}
--- packages/client/src/app/pages/main-page/main-page.component.ts
+++ packages/client/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
-import { SliderComponent } from '../../components/slider/slider.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ServicesProvidedComponent } from './components/services-provided/services-provided.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SliderComponent],
+  imports: [SliderComponent, ServicesProvidedComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
