2fd65ea65db5697a7bac5a11c7564b6774b685a3
yvrxslvw <yvrxslvv@gmail.com>
2024-09-01 23:27:25 +0300

    Delete routes enum

--- packages/client/src/app/app.routes.ts
+++ packages/client/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { APP_ROUTES } from './shared/constants/routes';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 
 export const routes: Routes = [
   {
-    path: APP_ROUTES.MAIN_PAGE,
+    path: '',
     component: MainPageComponent,
   },
 ];
--- packages/client/src/app/shared/constants/routes.ts
+++
@@ -1,3 +0,0 @@
-export const enum APP_ROUTES {
-  MAIN_PAGE = '',
-}
