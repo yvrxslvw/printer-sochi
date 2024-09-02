d091cf8cb332fbb16a378baee87f1db93846f03b
yvrxslvw <yvrxslvv@gmail.com>
2024-09-02 06:27:30 +0300

    Make about component

--- packages/client/src/app/pages/main-page/main-page.component.ts
+++ packages/client/src/app/pages/main-page/main-page.component.ts
@@ -4,6 +4,7 @@ import { SliderComponent } from './components/slider/slider.component';
 import { ServicesProvidedComponent } from './components/services-provided/services-provided.component';
 import { OperatingProcedureComponent } from './components/operating-procedure/operating-procedure.component';
 import { AdvantagesComponent } from './components/advantages/advantages.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-main-page',
@@ -13,6 +14,7 @@ import { AdvantagesComponent } from './components/advantages/advantages.componen
     ServicesProvidedComponent,
     OperatingProcedureComponent,
     AdvantagesComponent,
+    AboutComponent,
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
