173bbad2ca70c0f1c7718c6604d1b6bcda3f533f
yvrxslvw <yvrxslvv@gmail.com>
2024-09-02 06:16:44 +0300

    Make advantages component

---
+++ packages/client/src/app/pages/main-page/components/advantages/advantages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advantages',
+  standalone: true,
+  imports: [],
+  templateUrl: './advantages.component.html',
+  styleUrl: './advantages.component.scss',
+})
+export class AdvantagesComponent {}
--- packages/client/src/app/pages/main-page/main-page.component.ts
+++ packages/client/src/app/pages/main-page/main-page.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 
 import { SliderComponent } from './components/slider/slider.component';
 import { ServicesProvidedComponent } from './components/services-provided/services-provided.component';
-import { OperatingProcedureComponent } from "./components/operating-procedure/operating-procedure.component";
+import { OperatingProcedureComponent } from './components/operating-procedure/operating-procedure.component';
+import { AdvantagesComponent } from './components/advantages/advantages.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SliderComponent, ServicesProvidedComponent, OperatingProcedureComponent],
+  imports: [
+    SliderComponent,
+    ServicesProvidedComponent,
+    OperatingProcedureComponent,
+    AdvantagesComponent,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
